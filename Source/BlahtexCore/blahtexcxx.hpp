7c5450764724324fda38561ba4afe54a0ed2d327
Evan <evan@brucemcrooster.dev>
2026-01-08 14:42:56 -0500

    create root library header
    Includes all headers in the BlahtexCore folder

---
+++ Source/BlahtexCore/blahtexcxx.hpp
@@ -0,0 +1,13 @@
+// Include all headers in the BlahtexCore directory
+
+#include "InputSymbolTranslation.h"
+#include "Interface.h"
+#include "LayoutTree.h"
+#include "MacroProcessor.h"
+#include "Manager.h"
+#include "MathmlNode.h"
+#include "Misc.h"
+#include "Parser.h"
+#include "ParseTree.h"
+#include "Token.h"
+#include "XmlEncode.h"
